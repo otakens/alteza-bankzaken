c13c0618e8c13187d6a28d023a108af2e5fcc42e
otakens <otakens@gmail.com>
2017-10-10 17:03:01 +0200

    Add link to GitHub
    Set home as first page
    Add GitHub Link

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { OverviewComponent } from './overview/overview.component';
 import { PaymentComponent } from './payment/payment.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'overzicht', component: OverviewComponent },
   { path: 'betalen', component: PaymentComponent }
